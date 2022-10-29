e033602ff509704a6d034e1fa9c428fcaa2a3e14
SantiAR21 <santiago.armenta@cetis107.edu.mx>
2022-10-29 14:28:20 -0600

    tablas de alumnos

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,8 +9,42 @@
     <script src="{{asset('js/jquery-3.6.1.js')}}"></script>
 </head>
 <body>
-    <nav class="navbar navbar-light bg-light">
-    </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                Alumnos
+              </a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="{{url('/alumnos')}}">Consultar</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+
     <div class="container">
         <div class="row">
             <div class="col-12">
@@ -18,7 +52,7 @@
             </div>
         </div>
     </div>
-    <foster>MiEscuelita &copy; 2022</foster>        
-    <script src="{{asset('js/bootstrap.js)}}"></script>
+    <footer class="text-center">MiEscuelita &copy; 2022</footer>        
+    <script src="{{asset('js/bootstrap.js')}}"></script>
 </body>
 </html>
