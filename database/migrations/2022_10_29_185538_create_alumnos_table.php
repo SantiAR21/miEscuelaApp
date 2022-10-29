898747f0c41ee61e8e07fce9e202b497de88737a
SantiAR21 <santiago.armenta@cetis107.edu.mx>
2022-10-29 14:28:06 -0600

    Tabla de alumnos

---
+++ database/migrations/2022_10_29_185538_create_alumnos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alumnos', function (Blueprint $table) {
+            $table->id();
+            $table->String("no_control", 20);
+            $table->String("nombre", 300);
+            $table->integer("edad");
+            $table->boolean("sexo");
+            $table->date("fechaDeNacimiento");
+            $table->String("domicilio");
+            $table->String("telefono", 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alumnos');
+    }
+};
